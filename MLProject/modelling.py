27f3ca2871e67769908c84cd6ae54eb3f19165c4
Bondan Tri Wibowo <bondantriwibowo@gmail.com>
2025-06-18 08:52:52 +0700

    Update modelling.py dan workflow CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -19,35 +19,6 @@ def main(data_path):
         # Encode kolom kategorikal
         categorical_cols = X.select_dtypes(include=["object", "category"]).columns
         X = pd.get_dummies(X, columns=categorical_cols)
-<<<<<<< HEAD
-
-        # Split data
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-        # Train model
-        model = RandomForestRegressor(n_estimators=100, random_state=42)
-        model.fit(X_train, y_train)
-
-        # Prediksi dan log metrik
-        y_pred = model.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-        mlflow.log_metric("mse", mse)
-
-        # Logging model dengan signature dan input_example yang valid
-        input_example = X_test.iloc[:2].astype(float)  # konversi ke float untuk keamanan
-        signature = infer_signature(X_test, y_pred)
-
-        print("🚀 Logging model ke MLflow:")
-        print(input_example)
-
-        mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example,
-        signature=signature
-)
-
-=======
 
         # Split data
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -78,7 +49,6 @@ def main(data_path):
         # ✅ Simpan juga ke artifacts/ agar bisa diupload sebagai artefak
         os.makedirs("artifacts", exist_ok=True)
         joblib.dump(model, "artifacts/model.pkl")
->>>>>>> 3b949d1 (Revisi CI workflow: install joblib & upload artifact)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
